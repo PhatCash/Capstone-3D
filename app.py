d5df3fe959ef6aaab2944779ef2a8085f595c41e
Victor Castro <vicastro@pdx.edu>
2024-04-07 15:29:10 -0700

    Integrate JSON file

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 # Try to render one first - comment out nav bar
 @app.route('/')
 def printer_page():
-    return render_template("test.html")
-
+    # with closes the file
+    with open('./printer.json') as json_file:
+        return render_template("test.html", data=json.load(json_file)) # Send JSON data
+    
 if __name__ == '__main__':
     app.run()
