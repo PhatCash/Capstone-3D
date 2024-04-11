e9e0b9eed039a5f18187267e8331f8b831b23e32
Victor Castro <vicastro@pdx.edu>
2024-04-11 08:58:15 -0700

    Implement configure

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 # Try to render one first - comment out nav bar
 #@app.route('/')
 @app.route('/printer_status')
-def printer_status():
+def status():
     # with closes the file
     with open('./printer.JSON') as json_file:
         return render_template("printer_status.html", data=json.load(json_file)) # Send JSON data
     
 # Implement the other two webpages - need same JSON
+# Will change soon
 @app.route('/printer_page')
-def printer_page():
+def page():
     with open('./printer.JSON') as json_file: 
         return render_template("printer_page.html", data=json.load(json_file))
 
+@app.route('/configure_printer')
+def configure():
+    with open('./printer.JSON') as json_file:
+        return render_template("configure_printer.html", data=json.load(json_file))
 
 if __name__ == '__main__':
     app.run()
