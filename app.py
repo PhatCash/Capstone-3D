8a580cf4251198cffcd9ddd2503dc304da3118a5
Victor Castro <vicastro@pdx.edu>
2024-04-11 00:01:49 -0700

    Updates names & implemented printer_page

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ import json
 app = Flask(__name__)
 
 # Try to render one first - comment out nav bar
-@app.route('/')
-def printer_page():
+#@app.route('/')
+@app.route('/printer_status')
+def printer_status():
     # with closes the file
-    with open('./printer.json') as json_file:
-        return render_template("test.html", data=json.load(json_file)) # Send JSON data
+    with open('./printer.JSON') as json_file:
+        return render_template("printer_status.html", data=json.load(json_file)) # Send JSON data
     
+# Implement the other two webpages - need same JSON
+@app.route('/printer_page')
+def printer_page():
+    with open('./printer.JSON') as json_file: 
+        return render_template("printer_page.html", data=json.load(json_file))
+
+
 if __name__ == '__main__':
     app.run()
